06f95d33f261eed300e8dad157da6dad7fa27867
alex1212yang <alex_yang@tutk.com>
2018-03-19 13:57:49 +0800

    3.2
    3.2

--- src/junitpack/junit_oauth_login.java
+++ src/junitpack/junit_oauth_login.java
@@ -387,7 +387,7 @@ public class junit_oauth_login{
 				}
 				//ALLarraylist
 				ALLResultlist.add(Resultlist);
-
+/*
 		// keyin error password
 		driverChrome.findElement(By.id("id_email")).clear();
 		driverChrome.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
@@ -830,9 +830,9 @@ public class junit_oauth_login{
 		
 		
 		// °j°é }
-	}
+	}*/
 		driverChrome.close();  
 	    driverChrome.quit();
 		return ALLResultlist;
 	}
-}
+	}
